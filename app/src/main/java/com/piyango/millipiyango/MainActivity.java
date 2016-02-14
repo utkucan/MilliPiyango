0dd6d1c8fce25a8869cd529d0dd0a652e013d620
tkcn <utkucanyucel@gmail.com>
2016-02-14 23:25:17 +0200

    dropdown fill and populate

--- app/src/main/java/com/piyango/millipiyango/MainActivity.java
+++ app/src/main/java/com/piyango/millipiyango/MainActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -57,9 +58,8 @@ public class MainActivity extends Activity {
             public void onSuccess(String obj) {
                 try {
                     JSONArray mJsonArray = new JSONArray(obj);
-
-                    ArrayList<String> tarihList = new ArrayList<String>();
-                    JSONObject mJsonObject = new JSONObject();
+                    ArrayList<String> tarihList = new ArrayList<>();
+                    JSONObject mJsonObject ;
                     for (int i = 0; i < mJsonArray.length(); i++) {
                         mJsonObject = mJsonArray.getJSONObject(i);
                         tarihList.add(mJsonObject.getString("tarih"));
@@ -67,8 +67,59 @@ public class MainActivity extends Activity {
                     }
 
                     ArrayAdapter<CharSequence> adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item, tarihList);
-                    Spinner _EmpSpinner = (Spinner) findViewById(R.id.tarihSpinner);
-                    _EmpSpinner.setAdapter(adapter);
+                    Spinner spn = (Spinner) findViewById(R.id.tarihSpinner);
+                    spn.setAdapter(adapter);
+
+                    spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            RequestManager.getSayisal(new FetchJsonTask.Callback<SayisalSonuc>() {
+                                @Override
+                                public void onFail() {
+                                    Log.d("MilliPiyango", "HATA!");
+                                }
+
+                                @Override
+                                public void onStart() {
+                                    Log.d("MilliPiyango", "ONSTART");
+                                }
+
+                                @Override
+                                public void onSuccess(final SayisalSonuc obj) {
+                                    Log.d("MilliPiyango", "YAY!");
+                                    Log.d("MilliPiyango", obj.data.cekilisTarihi + "\t" + obj.data.rakamlarNumaraSirasi);
+                                    TextView v1 = (TextView) findViewById(R.id.sonucTextView);
+                                    v1.setText(obj.data.rakamlarNumaraSirasi);
+                                    ((TextView) findViewById(R.id.tarihTextView)).setText(obj.data.cekilisTarihi);
+                                }
+                            }, parent.getAdapter().getItem(position).toString());
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                            RequestManager.getSayisal(new FetchJsonTask.Callback<SayisalSonuc>() {
+                                @Override
+                                public void onFail() {
+                                    Log.d("MilliPiyango", "HATA!");
+                                }
+
+                                @Override
+                                public void onStart() {
+                                    Log.d("MilliPiyango", "ONSTART");
+                                }
+
+                                @Override
+                                public void onSuccess(final SayisalSonuc obj) {
+                                    Log.d("MilliPiyango", "YAY!");
+                                    Log.d("MilliPiyango", obj.data.cekilisTarihi + "\t" + obj.data.rakamlarNumaraSirasi + "\t" + obj.data.buyukIkrKazananIlIlceler.get(0).il);
+                                    TextView v1 = (TextView) findViewById(R.id.sonucTextView);
+                                    v1.setText(obj.data.rakamlarNumaraSirasi);
+                                    ((TextView) findViewById(R.id.tarihTextView)).setText(obj.data.cekilisTarihi);
+                                }
+                            }, parent.getAdapter().getItem(0).toString());
+                        }
+
+                    });
 
                 } catch (Exception e) {
 
